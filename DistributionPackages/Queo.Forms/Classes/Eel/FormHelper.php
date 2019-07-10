b0a6529b89716d71d0fd8d2af186014a90935764
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2019-07-10 15:46:14 +0200

    First functioning form fetch

--- DistributionPackages/Queo.Forms/Classes/Eel/FormHelper.php
+++ DistributionPackages/Queo.Forms/Classes/Eel/FormHelper.php
@@ -3,14 +3,33 @@
 
 namespace Queo\Forms\Eel;
 
-
+use Neos\Flow\Annotations as Flow;
 use Neos\Eel\ProtectedContextAwareInterface;
 
 class FormHelper implements ProtectedContextAwareInterface
 {
+
+    /**
+     * @Flow\InjectConfiguration("baseUrl")
+     * @var string
+     */
+    protected $baseUrl;
+
+    /**
+     * @Flow\InjectConfiguration("requestHeaders")
+     * @var array
+     */
+    protected $requestHeaders;
+
     public function fetchForm($formId)
     {
-        return "HALLO ICH WURDE AUFGERUFEN";
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET',  $this->baseUrl . '/forms/' . $formId . '.json', [
+            'headers' => $this->requestHeaders
+        ]);
+
+
+        return "HALLO ICH WURDE AUFGERUFEN" . $response->getBody()->getContents();
     }
 
     /**
