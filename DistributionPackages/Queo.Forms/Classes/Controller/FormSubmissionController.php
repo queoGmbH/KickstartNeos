1eee568ffe4b38ef5e4512267f1f2162c8056185
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2019-07-10 16:35:21 +0200

    Form Controller - added the route, we now properly generate the URL

---
+++ DistributionPackages/Queo.Forms/Classes/Controller/FormSubmissionController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Queo\Forms\Controller;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class FormSubmissionController extends ActionController
+{
+
+    public function submitAction()
+    {
+        die("RECEIVING FORM DATA");
+    }
+
+}
