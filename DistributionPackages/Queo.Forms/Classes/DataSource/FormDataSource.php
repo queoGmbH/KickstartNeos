44b799625777e24e7ce3e3d88791ae45c4dcfe5d
Anke Häslich <a.haeslich@queo-group.com>
2019-07-11 09:43:02 +0200

    start with client data source for select box

---
+++ DistributionPackages/Queo.Forms/Classes/DataSource/FormDataSource.php
@@ -0,0 +1,24 @@
+<?php
+namespace Queo\Forms\DataSource;
+
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\Neos\Service\DataSource\AbstractDataSource;
+
+class FormDataSource extends AbstractDataSource
+{
+
+    /**
+     * Get data
+     *
+     * The return value must be JSON serializable data structure.
+     *
+     * @param NodeInterface $node The node that is currently edited (optional)
+     * @param array $arguments Additional arguments (key / value)
+     * @return mixed JSON serializable data
+     * @api
+     */
+    public function getData(NodeInterface $node = null, array $arguments)
+    {
+        // TODO: Implement getData() method.
+    }
+}
